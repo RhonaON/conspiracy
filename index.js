3d81bd6cc335318b9ca012d78da43351d505d5d0
Rhona O'Neill <rhonaon@gmail.com>
2021-10-09 16:45:44 +1300

    add more conspiracy conten

--- index.js
+++ index.js
@@ -35,12 +35,12 @@ function getRandomArbitrary(min, max) {
 
 //Create input for beginning, middle and end
 
-let beginning = ["The Queen", "Nicolas Cage", "Bill Gates", "The Jews", "George Soros", "Jeff Bezos", "Jacinda Ardern", "Donald Trump", "The Chinese", "The Gays", "Space reptilians", "Feminists", "Science", "ANTIFA", "Big Pharma", "Oprah Winfrey", "McAuley Culkin", "Harambe", "Ke$ha", "Dr. Ashley Bloomfield", "Dance Moms", "Princess Diana", "9/11"]
+let beginning = ["The Queen", "Nicolas Cage", "Bill Gates", "The Jews", "George Soros", "Jeff Bezos", "Jacinda Ardern", "Donald Trump", "The Chinese", "The Gays", "Space reptilians", "Feminists", "Science", "ANTIFA", "Big Pharma", "Oprah Winfrey", "McAuley Culkin", "Harambe", "Ke$ha", "Dr. Ashley Bloomfield", "Dance Moms", "Princess Diana", "9/11", "the Catholic Church", "the Dalai Lama"]
 
 
 let middle = ["secretly owns", "controls", "is going to destroy", "is resposible for", "invented", "funds", "eats", "undermines good Christian values", "is building a communist agenda", "is stealing our divine energy source", "is running child sex rings", "coercing us to join a sex cult", "is putting 1080 in baby formula", "is contaminating the gene pool"]
 
-let end = ["babies", " with live laugh love propaganda", "with the bisexual agenda", "the global economy", "adrenochrome", "chem trails", "bitcoin", "NFTs", "the internet", "heroin", "for BIG DATA", "for oil", "with nuclear waste", "the facebook algorithm", "by teaching critical race theory in schools", "with affirmitive action", "with 5G", "by FORCING us to sneeze into our elbows", "with perpetual Briscoes sales", "with a sound set of sustainable development goals"]
+let end = ["babies", " with live laugh love propaganda", "with the bisexual agenda", "the global economy", "adrenochrome", "chem trails", "bitcoin", "NFTs", "the internet", "heroin", "for BIG DATA", "for oil", "with nuclear waste", "the facebook algorithm", "by teaching critical race theory in schools", "with affirmitive action", "with 5G", "by FORCING us to sneeze into our elbows", "with perpetual Briscoes sales", "with a sound set of sustainable development goals", "militant veganism"]
 
 
 //Create function that combines random beginning middle and end 'on-click' 
